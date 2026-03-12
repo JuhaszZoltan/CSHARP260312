69db64cc9c08056d3ef12b27f4bb279051526f13
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-03-12 20:09:14 +0100

    updt

---
+++ Citizen.cs
@@ -0,0 +1,49 @@
+﻿class Citizen(int id, int age, bool isDrinking, int? drinkingWeekdays, int? drinkingWeekends, bool gender, Nationality nationality, string netIncomeString, PopulationGroup populationGroup, string schoolDropout, string state, bool isTreated)
+{
+    public int Id { get; set; } = id;
+    public int Age { get; set; } = age;
+    public bool IsDrinking { get; set; } = isDrinking;
+    public int? DrinkingWeekDays { get; set; } = drinkingWeekdays;
+    public int? DrinkingWeekEnds { get; set; } = drinkingWeekends;
+    public bool Gender { get; set; } = gender;
+    public Nationality Nationality { get; set; } = nationality;
+    public (int? Min, int? Max) NetIncome
+    {
+        get
+        {
+            if (NetIncomeString == "Unknown" || NetIncomeString == "Refused") return (null, null);
+            if (NetIncomeString == "Under 1800") return (null, 1800);
+            if (NetIncomeString == "Above 32400") return (32400, null);
+            string[] tmp = NetIncomeString.Split(" to ");
+            return (int.Parse(tmp[0]), int.Parse(tmp[1]));
+        }
+    }
+    private string NetIncomeString { get; set; } = netIncomeString;
+    public PopulationGroup PopulationGroup { get; set; } = populationGroup;
+    public string SchoolDropout { get; set; } = schoolDropout;
+    public string State { get; set; } = state;
+    public bool IsTreated { get; set; } = isTreated;
+
+    public float? AverageDrinking
+    {
+        get
+        {
+            if (DrinkingWeekDays is null || DrinkingWeekEnds is null) return null;
+            return (DrinkingWeekDays * 5 + DrinkingWeekEnds * 2) / 7F;
+        }
+    }
+    public override string ToString() =>
+        $"\tCitizen ID: {Id}\n" +
+        $"\tAge: {Age}\n" +
+        $"\tDrinking: {(IsDrinking ? "YES" : "NO")}\n" +
+        $"\tDrinking on weekdays: {(DrinkingWeekDays is null ? "NO" : $"{DrinkingWeekDays} cl")}\n" +
+        $"\tDrinking on weekends: {(DrinkingWeekEnds is null ? "NO" : $"{DrinkingWeekEnds} cl")}\n" +
+        $"\tGender: {(Gender ? "Male" : "Female")}\n" +
+        $"\tNationality: {Nationality.Name}\n" +
+        $"\tNet income: {NetIncomeString} {(NetIncome.Min.HasValue || NetIncome.Max.HasValue ? "USD" : "")}\n" +
+        $"\tPopulation group: {PopulationGroup.Name}\n" +
+        $"\tDrop out from schoole: {SchoolDropout}\n" +
+        $"\tState: {State}\n" +
+        $"\tTreated: {(IsTreated ? "YES" : "NO")}\n" +
+        $"\tAverage drinking: {(AverageDrinking is null ? "NaN" : $"{AverageDrinking:0.00} lc")}";
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,94 @@
-﻿const string DrinksTxtPath = $"..\\..\\..\\data\\drinks.txt";
+﻿const string DrinksTxtPath = $"..\\..\\..\\data\\drink.txt";
+const string AsianTxtPath = $"..\\..\\..\\data\\asian.txt";
 
+List<Nationality> nationalities = [];
+List<PopulationGroup> populationGroups = [];
+List<Citizen> citizens = [];
+
+using StreamReader sr = new(DrinksTxtPath);
+_ = sr.ReadLine();
+int citId = 1;
+while(!sr.EndOfStream)
+{
+    string[] tmp = sr.ReadLine().Split(';');
+
+    int age = int.Parse(tmp[0]);
+    bool isDrinking = tmp[1] == "Yes";
+    int? drinkingWeekDays = tmp[2] == "NO" ? null : int.Parse(tmp[2]);
+    int? drinkingWeekEnds = tmp[3] == "NO" ? null : int.Parse(tmp[3]);
+    bool gender = tmp[4] == "Male";
+    int natId = int.Parse(tmp[5]);
+    Nationality nationlity = nationalities.SingleOrDefault(n => n.Id == natId);
+    if (nationlity is null)
+    {
+        nationalities.Add(new()
+        {
+            Id = natId,
+            Name = tmp[6],
+        });
+        nationlity = nationalities.Last();
+    }
+    string netIncomeString = tmp[7];
+
+    int popId = int.Parse(tmp[8]);
+
+    PopulationGroup populationGroup = populationGroups.SingleOrDefault(pg => pg.Id == popId);
+    if (populationGroup is null)
+    {
+        populationGroups.Add(new()
+        {
+            Id = popId,
+            Name = tmp[9],
+        });
+        populationGroup = populationGroups.Last();
+    }
+    string schoolDropout = tmp[10];
+    string state = tmp[11];
+    bool isTreated = tmp[12] == "1";
+
+
+    citizens.Add(new(id: citId, age, isDrinking, drinkingWeekDays, drinkingWeekEnds, gender, nationlity, netIncomeString, populationGroup, schoolDropout, state, isTreated));
+
+    citId++;
+}
+
+// Console.WriteLine($"nuber of citizens: {citizens.Count}");
+
+for (int i = 0; i < 5; i++) Console.WriteLine(citizens[i]+"\n");
+
+var f6a = citizens.Where(
+    c => c.IsDrinking
+    && c.Nationality.Name == "Birthright citizen"
+    && c.State == "Florida"
+    && c.Gender
+    && c.NetIncome.Min is not null
+    && c.NetIncome.Max is not null);
+
+Console.WriteLine("-----------------");
+
+foreach (var citizen in f6a) Console.WriteLine(citizen+"\n");
+
+var f6b = f6a.Min(c => c.NetIncome.Min);
+
+Console.WriteLine($"a leszurt allampolgarok minimalis jovedelme: {f6b} USD");
+
+
+var f7 = citizens.Where(
+    c => c.Age >= 18 && c.Age <= 65 
+    && c.PopulationGroup.Name == "Asian" 
+    && c.IsDrinking);
+
+using StreamWriter sw = new(AsianTxtPath);
+
+foreach (var citizen in f7) sw.WriteLine($"{citizen.Age},{citizen.State},{citizen.SchoolDropout}");
+
+Console.WriteLine($"{AsianTxtPath} kész!");
+
+//nexttime ->
+//LINQ(!!!)
+//using statement
+//top lvl calling
+//switch expression
+//lambda
+//is / is not
+//naming convention
